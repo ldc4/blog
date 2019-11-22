385f6dd095f7b92c29c44c7d743d9601ac74c84a
weedust <ldc4@vip.qq.com>
2019-11-22 14:35:40 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a25a39317231b9b8d373.js"
+    "url": "webpack-runtime-da461e2b61f3701695ea.js"
   },
   {
     "url": "app.198d93319ef7e0703812.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4d77739953ceba1be2377a29b1392517"
+    "revision": "f9af06116f1a2c2a82204ffb8e13a028"
   },
   {
     "url": "component---src-pages-404-js-f16571d51409aafd82b7.js"
